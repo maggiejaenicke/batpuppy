6b6b23a7074f4049c2187d8a0851b0f0240e1add
Maggie Jaenicke <maggiejaenicke@gmail.com>
2023-05-30 20:46:17 -0500

    Adds tabs and content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import './App.css';
 import BatpuppyHome from './Components/BatpuppyHome';
+import Tabs from "./Components/Tabs";
+import bear from './bear.png';
+import shoe from './shoe.png';
+import pear from './pear.png';
+import cockroach from './cockroach.png';
 
 function App() {
   return (
     <div className="App">
+            <div>
+                <img
+                src={bear}
+                alt="Thea's best bear"
+                width="100"
+                height="100"
+                />
+                <img
+                src={shoe}
+                alt="Momma's left shoe"
+                width="100"
+                height="100"
+                />
+                <img
+                src={pear}
+                alt="Pear"
+                width="100"
+                height="100"
+                />
+                <img
+                src={cockroach}
+                alt="Cockroach"
+                width="140"
+                height="115"
+                />
+            </div>
+      <Tabs>
+        <div label="Home">
+        <h3><a href="https://docs.google.com/forms/d/e/1FAIpQLScNpMEr-deodbKQIALfk4WUQxp7O0Sfpr4PaYLaoFe7ICJlQw/viewform" target="_blank" rel="noopener noreferrer">Sign up</a> for the Batpuppy mailing list to stay up to date!</h3>
+        <p>We'll send you news about our release date, pre-orders, events and more. We couldn't do it without you! </p>
+        </div>
+        <div label="About the Author">
+        <em>Katie Woodhouse</em> was born and raised in Portland, OR. She studied creative writing at Macalester College in Saint Paul, MN, and graduated mid-pandemic in 2021. Since then she has been a woodworker, a preschool art teacher, a barista, and a writer whenever she has the chance. She currently lives in South Minneapolis with 2 black cats and 2 human people. If she were a batdog, she would live in the trees by Cedar Lake!
+        </div>
+        <div label="About the Illustrator">
+        <em>Maggie Jaenicke</em> is from State College, PA, but has lived in the Twins Cities since 2017 where she Macalester College. She studied Geography and Environmental Studies. Quite frankly, she would rather be a bat cat, but don't tell anyone!
+        </div>
+        <div label="Take the Batpuppy Quiz!">
+        Find out what kind of batpuppy you are!
+        </div>
+      </Tabs>
       <header className="Batpuppy">
       </header>
       <BatpuppyHome
       puppy = 'batpuppy'
+      
       />
     </div>
   );
--- src/Components/BatpuppyHome.js
+++ src/Components/BatpuppyHome.js
@@ -1,27 +1,23 @@
 import React, { useEffect, useState } from "react";
 import batpuppy_cover from '../batpuppy_cover.png';
-import bear from '../bear.png';
-import shoe from '../shoe.png';
-import pear from '../pear.png';
-import cockroach from '../cockroach.png';
 
 export default function BatpuppyHome(props){
-    const [puppyName, setPuppyName] = useState('batpuppy')
-    const [counter, setCounter] = useState(0)
-    const [color, setColor] = useState('green')
-    const handleClick = () => {
-        setPuppyName('Brad')
-        setCounter(counter + 1)
-    }
-    const random_hex_color_code = () => {
-        let n = (Math.random() * 0xfffff * 1000000).toString(16);
-        return '#' + n.slice(0, 6);
-      };
+    const [emailAddress, setPuppyName] = useState('batpuppybook@gmail.com')
+    // const [counter, setCounter] = useState(0)
+    // const [color, setColor] = useState('green')
+    // const handleClick = () => {
+    //     setPuppyName('Brad')
+    //     setCounter(counter + 1)
+    // }
+    // const random_hex_color_code = () => {
+    //     let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    //     return '#' + n.slice(0, 6);
+    //   };
       //console.log(random_hex_color_code())
 
-    useEffect(()=>{
-        setColor(random_hex_color_code)
-    }, [counter])
+    // useEffect(()=>{
+    //     setColor(random_hex_color_code)
+    // }, [counter])
 
     useEffect(()=>{
 
@@ -29,44 +25,17 @@ export default function BatpuppyHome(props){
     return (
         <div>
         <br></br>
-            <div >
-                <img
-                src={bear}
-                alt="Thea's best bear"
-                width="100"
-                height="100"
-                />
-                <img
-                src={shoe}
-                alt="Momma's left shoe"
-                width="100"
-                height="100"
-                />
-                <img
-                src={pear}
-                alt="Pear"
-                width="100"
-                height="100"
-                />
-                <img
-                src={cockroach}
-                alt="Cockroach"
-                width="140"
-                height="115"
-                />
-            </div>
-        <h1>This is Batpuppy's Home!</h1>
-        <h3><a href="https://docs.google.com/forms/d/e/1FAIpQLScNpMEr-deodbKQIALfk4WUQxp7O0Sfpr4PaYLaoFe7ICJlQw/viewform" target="_blank" rel="noopener noreferrer">Sign up</a> for the Batpuppy mailing list to stay up to date!</h3>
+        {/* <h1>This is Batpuppy's Home!</h1>
         <p>Puppy's name is {puppyName}</p>
         <p>I've clicked {counter} times</p>
-        <button onClick={handleClick} style={{color: color}}>Change BP's name to Brad</button><br></br><br></br>
-        {/* <button onClick={handleClick} style={{color: 'black'}}></button><br></br> */}
+        <button onClick={handleClick} style={{color: color}}>Change BP's name to Brad</button><br></br><br></br> */}
         <img 
         src={batpuppy_cover} 
         alt="Batpuppy flying with girl"
         width="330"
         height="300"
         />
+        <p>Questions? Reach out to us: <a href="mailto:batpuppybook@gmail.com?bcc=maggiejaenicke@gmail.com,kwoodhouse1@gmail.com" target="_blank" rel="noopener noreferrer">{emailAddress}</a></p>
         </div>
     )
 }
